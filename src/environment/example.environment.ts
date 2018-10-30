96052c7d1c08cb7bf1f3e4d029395e95108bc4d4
Colin Bean <colin@missionbit.com>
2018-10-30 10:34:47 -0700

    Added example environment file

---
+++ src/environment/example.environment.ts
@@ -0,0 +1,16 @@
+/*
+For your project, copy this file to environment.ts in the same directory
+and fill in the config vaules below with the configuration from Firebase
+*/
+
+export const environment = {
+    production: false,
+    firebase: {
+        apiKey: "",
+        authDomain: "",
+        databaseURL: "",
+        projectId: "",
+        storageBucket: "",
+        messagingSenderId: ""
+    }
+  };
